d3f05c0ff4900389a0b073c8389068374baf4c9c
Nebesssa2000 <114442522+Nebesssa2000@users.noreply.github.com>
2022-10-18 09:57:58 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,30 +8,4 @@ public class Test extends Main {
         String str = in.next();
         calc(str);
     }
-
-//    public static String calc(String str){
-//
-//        int result = 0;
-//        String[] numbers;
-//
-//        if (str.indexOf('+') > 0) {
-//            numbers = str.split("[+]");
-//            result = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
-//        } else if (str.indexOf('-') > 0) {
-//            numbers = str.split("[-]");
-//            result = Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]);
-//        } else if (str.indexOf('/') > 0) {
-//            numbers = str.split("[/]");
-//            result = Integer.parseInt(numbers[0]) / Integer.parseInt(numbers[1]);
-//        } else if (str.indexOf('*') > 0) {
-//            numbers = str.split("[*]");
-//            result = Integer.parseInt(numbers[0]) * Integer.parseInt(numbers[1]);
-//        } else {
-//
-//        }
-//
-//        System.out.println(result);
-//
-//    return String.valueOf(result);
-//    }
 }
